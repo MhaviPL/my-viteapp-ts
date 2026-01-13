081a193e87565928bfcaa6f355a7f5486c02d12a
MhaviPL <matheuslp330@gmail.com>
2026-01-13 11:43:57 -0300

    Ciando o component Card e utilizando o Fragment para utilizar duas divs irmãs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Card } from "./components/Card"
+import { Fragment } from "react/jsx-runtime"
+
 function App() {
   return(
+    <Fragment>
     <div>
       Hello Word
+      <Card/>
+      <Card/>
     </div>
+    <div>
+      Footer
+    </div>
+    </Fragment>
   )
 }
 
